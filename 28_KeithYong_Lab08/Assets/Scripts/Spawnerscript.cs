5164275e602024308849b5b3af3ba3c62fcae3db
KeithY14 <keithyong14@yahoo.com.sg>
2021-10-20 10:59:01 +0800

    Game Over scene

--- 28_KeithYong_Lab08/Assets/Scripts/Spawnerscript.cs
+++ 28_KeithYong_Lab08/Assets/Scripts/Spawnerscript.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class Spawnerscript : MonoBehaviour
 {
-    public GameObject SpawnObject;
-    public GameObject SpawnObject2;
-    public GameObject SpawnObject3;
+    public GameObject[] SpawnObject;
     float PositionY;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("SpawnObjects", 1, 1);
-        InvokeRepeating("SpawnObjects2", 3, 3);
-        InvokeRepeating("SpawnObjects3", 5, 5);
     }
 
     // Update is called once per frame
@@ -28,21 +24,8 @@ public class Spawnerscript : MonoBehaviour
     void SpawnObjects()
     {
         PositionY = Random.Range(4, -4f);
+        int random = Random.Range(0, SpawnObject.Length);
         this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject, transform.position, transform.rotation);
-    }
-
-    void SpawnObjects2()
-    {
-        PositionY = Random.Range(7, -7f);
-        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject2, transform.position, transform.rotation);
-    }
-
-    void SpawnObjects3()
-    {
-        PositionY = Random.Range(10, -10f);
-        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
-        Instantiate(SpawnObject3, transform.position, transform.rotation);
+        Instantiate(SpawnObject[random], transform.position, transform.rotation);
     }
 }
